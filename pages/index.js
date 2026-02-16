942475ff3c285d1b3f237258e733ed326224fcf0
MikaelHenrique <el.mh506@gmail.com>
2026-02-16 20:00:32 -0300

    change phrase

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
             <h1>Oii</h1>
             <p>
                 Percebeu como está chuvos hoje!!<br/>
-                Não falo apenas do tempo... Só que a forma como olhamos para essa chuva faz toda a diferença!
+                Não falo apenas do tempo... Só que a forma como olhamos para essa chuva faz toda a diferença!!
             </p>
         </>
     )

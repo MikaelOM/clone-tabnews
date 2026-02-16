a070064d25a182f76939b66da4c1bb398d703563
MikaelHenrique <el.mh506@gmail.com>
2026-02-16 18:55:17 -0300

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <>
+            <h1>Oii</h1>
+            <p>
+                Percebeu como está chuvos hoje!!<br/>
+                Não falo apenas do tempo... Só que a forma como olhamos para essa chuva faz toda a diferença!
+            </p>
+        </>
+    )
+}
+
+export default Home

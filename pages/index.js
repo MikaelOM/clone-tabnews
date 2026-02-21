bf0f8e14f10659db13d6a3d6c46396090297ef63
MikaelHenrique <el.mh506@gmail.com>
2026-02-20 21:37:19 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 function Home() {
-    return (
-        <>
-            <h1>Oii</h1>
-            <p>
-                Percebeu como está chuvos hoje!!<br/>
-                Não falo apenas do tempo... Só que a forma como olhamos para essa chuva faz toda a diferença!!
-            </p>
-        </>
-    )
+  return (
+    <>
+      <h1>Oii</h1>
+      <p>
+        Percebeu como está chuvos hoje!!
+        <br />
+        Não falo apenas do tempo... Só que a forma como olhamos para essa chuva
+        faz toda a diferença!!
+      </p>
+    </>
+  );
 }
 
-export default Home
+export default Home;
